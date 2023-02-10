30a2df65e8619a1d637bd206f6d9205ae7a8051a
MahmdulHasan <mahmudul92hasan@gmail.com>
2023-02-10 14:03:41 +0600

    added dbcontext & repository for data access

---
+++ src/Libraries/Intus.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace Intus.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);    
+        }
+    }
+}
---
+++ src/Libraries/Intus.Data/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intus.Data.Repository
+{
+    public partial interface IRepository<TEntity> where TEntity : class
+    {
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        IQueryable<TEntity> Entity { get; }
+        IQueryable<TEntity> EntityWithNoTracking { get; }
+    }
+}

ad1da77e80453e742b79e2f5976911724a0099fd
MahmdulHasan <mahmudul92hasan@gmail.com>
2023-02-10 14:50:55 +0600

    added dependent project reference and registered dbcontext and repository classes in DI container

---
+++ src/Presentation/Intus.Web.Framework/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Intus.Data;
+using Intus.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Intus.Web.Framework.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+          
+            services.AddDbContext<ApplicationDbContext>(options =>
+            
+                options.UseSqlServer(configuration.GetConnectionString("ApplicationConnection"))
+            );
+
+
+            services.AddScoped(typeof(IRepository<>), typeof(ApplicationDbRepository<>));
+        
+        }
+    }
+}

36765ca887f7e3ee558f69f2f89500e1427fa6af
MahmdulHasan <mahmudul92hasan@gmail.com>
2023-02-12 03:28:18 +0600

    implemented order add and edit operation without api integration

---
+++ src/Presentation/Intus.Web.Framework/Contracts/V1/Order/WindowModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intus.Web.Framework.Contracts.V1.Order
+{
+    public class WindowModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public List<SubElementModel> SubElements { get; set; } = new List<SubElementModel>();
+    }
+}
